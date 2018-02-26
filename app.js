6eac27e8f8ec7248fe4eb87c7f96815919fb8137
Ido <idoprv@gmail.com>
2018-02-26 23:51:20 +0200

    start support to mongo,rest and socket (buggy)

--- app.js
+++ app.js
@@ -1,53 +1,29 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var http2 = require('http').Server(express);
-var io = require('socket.io')(http2);
-require('./database_connector');
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var db = require('./database_connector')(app);
 
-var gameRouter = require('./routes/game');
+server.listen(1234);
 
-var app = express();
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-gameRouter.register(app, '/game');
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.get('/', function (req, res) {
+	res.sendfile(__dirname + '/index.html');
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.send('failure');
+io.on('connection', function (socket) {
+	console.log("user connected");
+	socket.on('updated color', function (data) {
+		console.log(data);
+	});
 });
 
 
-io.on('connection', function(socket){
-    console.log('a user connected');
-    socket.on('updated color', function(newColor){
-        console.log('updated color to: ' + newColor)
-    })
+db.game.insert({title: "Mako-Game", state:"Open"});
+db.game.insert({title: "Mako-Kofiko", state:"Closed"});
+
+
+io.on('GetGames', function(socket){
+	var openGames = db.Game.find({state: "Open"});
+	io.emit(openGames);
 });
-
-
 
 module.exports = app;
--- database_connector.js
+++ database_connector.js
@@ -1,25 +1,45 @@
-var mongoose = require('mongoose');
+var restful = require('node-restful'),
+	mongoose = restful.mongoose;
 
-var connectionString = "mongodb://localhost";
-
+var connectionString = "mongodb://drawnuts:1q2w3e$R@ds115546.mlab.com:15546/draw_nuts";
 mongoose.connect(connectionString);
 
 var db = mongoose.connection;
-
 db.on('error', console.error.bind(console, 'connection error:'));
 
 db.once('open', function () {
-    console.log("DB connection alive");
+	console.log("DB connection alive");
 });
 
-var gameSchema = mongoose.Schema({
-    name: {type: String, required: true},
-    // player: {type: Schema.ObjectId, ref: 'player', require: true}
-});
+module.exports = function(app)
+{
 
-gameSchema.methods.CreateGame  = function(){
-    var welcome = "New Game Created: " + this.name;
-    console.log(welcome);
+	var Game = app.resource = restful.model('game', mongoose.Schema({
+		title: String,
+		state: String
+	})).methods(['get', 'post', 'put', 'delete']);
+
+	var User = app.resource = restful.model('user', mongoose.Schema({
+		username: String,
+		password: String
+	})).methods(['get', 'post', 'put', 'delete']);
+
+	User.route('login', function(req, res) {
+		var loginUser = User.find(
+			{
+				userName: req.userName,
+				password: req.password
+			});
+		if(!!loginUser)
+			res.send(false);
+		res.send(true);
+	});
+
+	User.register(app, '/user');
+	Game.register(app, '/game');
+
+	return {
+		game: Game,
+		user: User
+	}
 };
-
-module.exports = gameSchema;
